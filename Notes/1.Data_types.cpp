b980cae2f08ec32dab366e5b0fc3c1729fa59bd2
Aarsh kolhe <aarshkolhe01@gmail.com>
2026-05-05 23:34:00 +0530

    data type is covered

---
+++ Notes/1.Data_types.cpp
@@ -0,0 +1,17 @@
+/*
+
+[+]Data types
+Data types specifies the type and size of the data a variable can store
+
+[+]Types of Data type
+
+1. Primitive(Build-in Data types)
+These are predefined by C++ and represnt basic values
+
+Sr Data type     Keyword     size     range
+1. Integer       int         4bytes  -2,147,483,648 to 2,147,483,647 approx -2billion to +2billion
+2. 
+2. Derived
+3. User defined
+
+*/
