f343d3fabc49d649016591a9e916bdea1ff46f39
Aarsh kolhe <aarshkolhe01@gmail.com>
2026-05-11 00:38:35 +0530

    primitive data type is covered

--- Notes/1.Data_types.cpp
+++ Notes/1.Data_types.cpp
@@ -3,15 +3,54 @@
 [+]Data types
 Data types specifies the type and size of the data a variable can store
 
+
 [+]Types of Data type
 
 1. Primitive(Build-in Data types)
-These are predefined by C++ and represnt basic values
+->These are predefined by C++ and represnt basic values
 
-Sr Data type     Keyword     size     range
-1. Integer       int         4bytes  -2,147,483,648 to 2,147,483,647 approx -2billion to +2billion
-2. 
+Sr Data type       Keyword     size     range/description
+1. Integer         int         4bytes  -2,147,483,648 to 2,147,483,647 approx -2billion to +2billion
+2. Character       char        1byte   A-Z and a-z with special symbols
+3. Boolean         bool        1byte   true or false(1 or 0)
+4. Floating point  float       4byte   it can have 7 digits decimal
+5. Void            void        0byte   it has no values
+
+Now there are some variations you can use with these data types:-
+
+a.Integers
+
+Sr   Integer Types            typical size   range of values
+1.   short int                2byte          -32,768 to 32,767
+2.   unsigned short int       2byte          0 to 65,535
+3.   unsigned int             4byte          0 to 2,294,967,295
+4.   long int                 4 or 8bytes    same as int or long int
+5.   unsigned long int        4 or 8bytes    same as unsigned int or long long
+6.   long long int            8bytes         -9.22*10^18 to 9.22*10^18
+7.   unsigned long long int   8bytes          0 to 1.84*10^19
+
+
+b.Character
+
+Sr   Integer Types            typical size   range of values
+1.   char                     1byte           -128 to 127
+2.   signed char              1byte           -128 to 127
+3.   unsigned char            1byte            0 to 255
+4.   wchar_t                  2 or 4 bytes     0 to 65,535 or 0 to 4,294,967,295
+5.   char16_t                 2byte            0 to 65,535(UTF-16 character)
+6.   char32_t                 4byte            0 to 4,294,967,295(UTF-32 character)
+
+c.floating-point 
+
+Sr   Integer Types            typical size   range of values
+1.   float                    4byte          -7 decimal digit
+2.   double                   8bytes         -15 decimal digit
+3.   long double              12 or 16bytes  -18 to 33 decimal digits
+
+
 2. Derived
+
+
 3. User defined
 
 */
